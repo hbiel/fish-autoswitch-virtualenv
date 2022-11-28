19ab3041f2adf626e6f3c9949337a453e2f42199
Helge Biel <18981810+hbiel@users.noreply.github.com>
2022-11-28 19:28:31 +0000

    fix: autoswitch not activating on start of shell

--- conf.d/_autovenv.fish
+++ conf.d/_autovenv.fish
@@ -5,9 +5,9 @@ if status is-interactive
     # erase the variable VIRTUAL_ENV when entering fish to clean up potential leftovers (e.g. when doing exec fish in a virtualenv)
     set -e VIRTUAL_ENV
 
-    function _autovenv_activate --on-variable dirprev # PWD get's updated before dirprev so we're listening on dirprev instead to ensure that it is up-to-date
+    function _autovenv_activate_on_cd --on-variable dirprev # PWD get's updated before dirprev so we're listening on dirprev instead to ensure that it is up-to-date
         test -z "$dirprev" && return # dirprev get's set on first cd and then updated; this fires the trigger two times, so we return early
         _autovenv_check_venv
     end
-    _autovenv_activate
+    _autovenv_check_venv
 end
